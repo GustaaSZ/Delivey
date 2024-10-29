b2b40eb8590f190d36c212e6528560a6e11ec203
GustaaSz <gugaabrantes2015@gmail.com>
2024-10-28 22:35:38 -0300

    feat: adding responsive banner screen

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -3,7 +3,7 @@ import PagerView from 'react-native-pager-view'
 
 export function Banner() {
  return (
-   <View className='w-full h-36 rounded-2xl mt-8 mb-4'>
+   <View className='w-full h-36 md:h-60 rounded-2xl mt-8 mb-4'>
     {/* Aplicamos o style direto no componente pois ele n reconhece o className */}
         <PagerView style={{ flex: 1 }} initialPage={0} pageMargin={14}>
             <Pressable 
@@ -13,7 +13,7 @@ export function Banner() {
             >
                 <Image
                     source={require("../../assets/banner1.png")}
-                    className='w-full h-36 rounded-2xl'
+                    className='w-full h-36 md:h-60 rounded-2xl'
                 />
             </Pressable>
 
@@ -24,7 +24,7 @@ export function Banner() {
             >
                 <Image
                     source={require("../../assets/banner2.png")}
-                    className='w-full h-36 rounded-2xl'
+                    className='w-full h-36 md:h-60 rounded-2xl'
                 />
             </Pressable>
             
@@ -36,7 +36,7 @@ export function Banner() {
             >
                 <Image
                     source={require("../../assets/banner3.png")}
-                    className='w-full h-36 rounded-2xl'
+                    className='w-full h-36 md:h-60 rounded-2xl'
                 />
             </Pressable>
 
@@ -47,7 +47,7 @@ export function Banner() {
             >
                 <Image
                     source={require("../../assets/banner4.png")}
-                    className='w-full h-36 rounded-2xl'
+                    className='w-full h-36 md:h-60 rounded-2xl'
                 />
             </Pressable>
 
@@ -58,7 +58,7 @@ export function Banner() {
             >
                 <Image
                     source={require("../../assets/banner5.png")}
-                    className='w-full h-36 rounded-2xl'
+                    className='w-full h-36 md:h-60 rounded-2xl'
                 />
             </Pressable>
             
