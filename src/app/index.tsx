16d8f6d45429c04e3101b7bfc8b02e9117d8c93d
GustaaSz <gugaabrantes2015@gmail.com>
2024-10-28 21:14:56 -0300

    adding the updates to run the project with tailwind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>The firts app screen - Gustavo Abrantes</Text>
+    <View className="flex-1 justify-center w-full h-full items-center">
+      <Text className="text-black font-semibold">The firts app screen - Gustavo Abrantes</Text>
     </View>
   );
 }
