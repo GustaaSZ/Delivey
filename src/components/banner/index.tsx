80fcc4542f9e08fd2b1efb00ed9603e3a286b3a6
GustaaSz <gugaabrantes2015@gmail.com>
2024-10-28 23:54:34 -0300

    feat: adding auto-scrolling to banners

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,68 +1,56 @@
+import { useEffect, useRef, useState } from 'react';
 import { View, Pressable, Image } from 'react-native';
 import PagerView from 'react-native-pager-view'
 
 export function Banner() {
- return (
-   <View className='w-full h-36 md:h-60 rounded-2xl mt-8 mb-4'>
-    {/* Aplicamos o style direto no componente pois ele n reconhece o className */}
-        <PagerView style={{ flex: 1 }} initialPage={0} pageMargin={14}>
-            <Pressable 
-                className='w-full h-36 rounded-2xl' 
-                key="1"
-                onPress={() => console.log("Clicou no Banner")}
-            >
-                <Image
-                    source={require("../../assets/banner1.png")}
-                    className='w-full h-36 md:h-60 rounded-2xl'
-                />
-            </Pressable>
+    const pagerRef = useRef<PagerView>(null);
+    const totalPages = 5;
 
-            <Pressable 
-                className='w-full h-36 rounded-2xl' 
-                key="2"
-                onPress={() => console.log("Clicou no Banner")}
-            >
-                <Image
-                    source={require("../../assets/banner2.png")}
-                    className='w-full h-36 md:h-60 rounded-2xl'
-                />
-            </Pressable>
-            
+    // Array com os caminhos das imagens
+    const bannerImages = [
+        require('../../assets/banner1.png'),
+        require('../../assets/banner2.png'),
+        require('../../assets/banner3.png'),
+        require('../../assets/banner4.png'),
+        require('../../assets/banner5.png')
+    ];
 
-            <Pressable 
-                className='w-full h-36 rounded-2xl' 
-                key="3"
-                onPress={() => console.log("Clicou no Banner")}
-            >
-                <Image
-                    source={require("../../assets/banner3.png")}
-                    className='w-full h-36 md:h-60 rounded-2xl'
-                />
-            </Pressable>
+    // Estado para controlar a página ativa
+    const [pageIndex, setPageIndex] = useState(0);
 
-            <Pressable 
-                className='w-full h-36 rounded-2xl' 
-                key="4"
-                onPress={() => console.log("Clicou no Banner")}
-            >
-                <Image
-                    source={require("../../assets/banner4.png")}
-                    className='w-full h-36 md:h-60 rounded-2xl'
-                />
-            </Pressable>
+    useEffect(() => {
+        const interval = setInterval(() => {
+          // Atualiza o índice para a próxima página
+          setPageIndex((prevIndex) => (prevIndex + 1) % totalPages);
+        }, 3000);
+    
+        return () => clearInterval(interval); // Limpa o intervalo ao desmontar o componente
+      }, []);
+    
+      // Atualiza a página no PagerView sempre que o pageIndex mudar
+      useEffect(() => {
+        if (pagerRef.current) {
+          pagerRef.current.setPage(pageIndex); // Garante que setPage existe e é chamado apenas quando pagerRef está definido
+        }
+      }, [pageIndex]);
 
-            <Pressable 
-                className='w-full h-36 rounded-2xl' 
-                key="5"
-                onPress={() => console.log("Clicou no Banner")}
-            >
-                <Image
-                    source={require("../../assets/banner5.png")}
-                    className='w-full h-36 md:h-60 rounded-2xl'
-                />
-            </Pressable>
-            
-        </PagerView>
-   </View>
-  );
+    return (
+        <View className='w-full h-36 md:h-60 rounded-2xl mt-8 mb-4'>
+            {/* Aplicamos o style direto no componente pois ele n reconhece o className */}
+            <PagerView ref={pagerRef} style={{ flex: 1 }} initialPage={0} pageMargin={14}>
+                {bannerImages.map((image, index) => (
+                    <Pressable 
+                        className='w-full h-36 rounded-2xl' 
+                        key={index}
+                        onPress={() => console.log(`Clicou no Banner ${index + 1}`)}
+                    >
+                        <Image
+                            source={image}
+                            className='w-full h-36 md:h-60 rounded-2xl'
+                        />
+                    </Pressable>
+                ))}
+            </PagerView>
+        </View>
+    );
 }
