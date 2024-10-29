7afb4972b22437dabc0122a5764300055b61c580
GustaaSz <gugaabrantes2015@gmail.com>
2024-10-29 10:09:50 -0300

    feat: adding navigation whit drawer menu and adding more one page to navigate

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,48 @@
 import '../styles/global.css'
 import { Slot } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+import { Drawer } from "expo-router/drawer"
+import { Feather, Ionicons } from '@expo/vector-icons';
 
 export default function RootLayout() {
   return (
-    <Slot/>
+    // <Slot/>
+    <GestureHandlerRootView>
+      <Drawer 
+        screenOptions={{
+          headerShown: false,
+          drawerStyle: {backgroundColor: '#3f3f46'} // define a cor do drawer
+        }}
+      >
+          {/* Estilização do Icone */}
+          <Drawer.Screen name='index' options={{ 
+            drawerLabel: "Home", // Nome personalizado
+            drawerIcon: ({ focused }) => ( // Logica pra que ele fique preenchido quando clicado
+              <Ionicons 
+                name={focused ? 'home' : 'home-outline'} 
+                color={focused ? '#d4d4d8' : '#e4e4e7'} 
+                size={18} 
+              />
+            ),
+          }}
+          />
+
+        <Drawer.Screen 
+          name="notifications" 
+          options={{ 
+            drawerLabel: "Notificações", 
+            drawerIcon: ({ focused }) => (
+              <Ionicons 
+                name={focused ? 'notifications' : 'notifications-outline'} 
+                color={focused ? '#d4d4d8' : '#e4e4e7'} 
+                size={18} 
+              />
+            ),
+          }}
+        />
+      </Drawer>
+      
+    </GestureHandlerRootView>
+    // </Slot>
   );
 }
---
+++ src/app/notifications.tsx
@@ -0,0 +1,35 @@
+import { Text, View, ScrollView } from "react-native";
+import { Header } from "../components/header";
+import Constants from 'expo-constants'
+import { Banner } from "../components/banner";
+import { Search } from "../components/search";
+import { Section } from "../components/section";
+
+const statusBarHeight = Constants.statusBarHeight;
+// px = padding interno na esquerda e na direita
+
+export default function Notifications() {
+  return (
+    <ScrollView 
+      style={{ flex: 1}} 
+      className="bg-zinc-900 text-zinc-300"
+      showsVerticalScrollIndicator={false}
+    >
+      <View className="w-full px-6" style={{ marginTop: statusBarHeight + 15}}>
+        <Header/>
+
+        {/* <Banner/> */}
+        
+        <Search/>
+
+        {/* <Section 
+          name="Comidas em Alta"
+          size="text-2xl"
+          label="Veja mais"
+          action={ () => console.log("Clicou no veja mais")}
+        
+        /> */}
+      </View>
+    </ScrollView>
+  );
+}
