e8f65960cf9b9f252f9f5ef7000d1cd302ba7bd4
GustaaSz <gugaabrantes2015@gmail.com>
2024-11-01 11:28:27 -0300

    feat: adding the navigation to the food Details screen

--- src/app/foodDetails.tsx
+++ src/app/foodDetails.tsx
@@ -1,15 +1,23 @@
-// import { router } from 'expo-router';
-// import { View, Text, Image } from 'react-native';
+import { router, useRouter } from 'expo-router';
+import { View, Text, Image, Pressable } from 'react-native';
+import BackButton from '../components/backButton';
+import { useLocalSearchParams } from 'expo-router';
 
-// export default function FoodDetails( ) {
-//   const { id } = router.params;
 
-//   return (
-//     <View className="flex-1 items-center justify-center bg-zinc-900">
-//       <Image source={{ uri: id.image }} style={{ width: 200, height: 150, borderRadius: 10 }} />
-//       <Text className="text-green-400 text-2xl mt-4">R$ {id.price}</Text>
-//       <Text className="text-zinc-100 text-xl font-semibold">{id.name}</Text>
-//       <Text className="text-zinc-300 text-lg mt-2">{id.time}</Text>
-//     </View>
-//   );
-// }
+export default function FoodDetails() {
+    const { id } = useLocalSearchParams(); // Acessa o ID do alimento
+    
+    // Exemplo de como você pode usar o ID para buscar o alimento
+    // const food = findFoodById(id); // Função fictícia para buscar o alimento pelo ID
+
+    return (
+        <View className="flex-1 items-center justify-center bg-zinc-900 relative">
+            <BackButton />
+            <Text className='text-zinc-200 text-2xl text-center'>Detalhes do Alimento</Text>
+            {/* Exemplo de renderização dos detalhes do alimento */}
+            <Text className='text-zinc-400'>ID do alimento: {id}</Text>
+            {/* <Text className='text-zinc-400'>ID do restaurante: {}</Text> */}
+            {/* Renderize aqui as informações específicas do alimento */}
+        </View>
+    );
+}
