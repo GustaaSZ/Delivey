5ed3ed9dc443eb78e1f9e8962193bb66f874e00b
GustaaSz <gugaabrantes2015@gmail.com>
2024-11-04 22:20:02 -0300

    style: upddating the the food details screen style

--- src/app/foodDetails.tsx
+++ src/app/foodDetails.tsx
@@ -6,7 +6,6 @@ import Constants from 'expo-constants';
 import { ScrollView } from 'react-native-gesture-handler';
 import { Section } from '../components/section';
 import { useEffect, useState } from 'react';
-import { RestaurantProps as TrendingRestaurantProps } from '../components/trendingRe';
 
 // Constante pra definir uma altura padrão na view
 const statusBarHeight = Constants.statusBarHeight;
@@ -66,7 +65,7 @@ export default function FoodDetails() {
                     source={{ uri: imageUri }}
                     style={{
                         width: '100%',
-                        height: 200,  // Defina a altura que você deseja para a imagem de fundo
+                        height: 200,  // Defina a altura 
                         justifyContent: 'flex-start',
                         paddingTop: statusBarHeight + 15,
                     }}
@@ -79,7 +78,7 @@ export default function FoodDetails() {
                
                 {/* Detalhes do alimento abaixo da imagem */}
                 <ScrollView 
-                    style={{ paddingHorizontal: 24, paddingVertical: 16}}
+                    style={{ paddingHorizontal: 10, paddingVertical: 16}}
                     className="bg-zinc-900 text-zinc-300"
                     showsVerticalScrollIndicator={false}
                 >
@@ -90,38 +89,52 @@ export default function FoodDetails() {
                         action={ () => console.log("Clicou no veja mais")}
                     />
                     <View className='px-4'>
-                        <Text className='text-zinc-400'>Id do alimento: {id}</Text>
-                        <Text className='text-zinc-400'>Preço: R$ {price}</Text>
-                        <Text className='text-zinc-400'>Ingredientes: {ingredientes}</Text>
-                        <Text className='text-zinc-400'>Id do restaurante: {restaurantId}</Text>
+                        <Text className='text-zinc-400 text-lg'>{ingredientes}</Text>
+                        <Text className='text-zinc-300 text-lg' style={{marginTop: 15}}>Preço: R$ {price}</Text>
+                    </View>
 
                         {/* DETALHES DO RESTAURANTE */}
                         {restaurant ? (
-                            <View style={{ marginTop: 16 }}>
-                                <Text className='text-zinc-100 text-xs'>Vendido por: {restaurant.name}</Text>
-                                <Text className='text-zinc-100 text-xs'>id do restaurante: {restaurant.id}</Text>
-                                <Image
-                                    source={{ uri: restaurant.image }}
-                                    // className='w-20 h-20 rounded-full'
-                                    style={{ width: 100, height: 100, borderRadius: 50 }}
-                                />
-                                <Text className='text-zinc-100 text-xl mt-1'>Mais perto de sua localização Atual:</Text>
-                                <Text className='text-zinc-400 text-xs'>{restaurant.name} de Taguatinga </Text>
-                                <Image
-                                    source={{ uri: restaurant.imageLocal }}
-                                    style={{
-                                        width: '100%',
-                                        height: 180,  // Defina a altura que você deseja para a imagem de fundo
-                                        justifyContent: 'center',
-                                        paddingTop: statusBarHeight + 15,
-                                        borderRadius: 25
-                                    }}
-                                />
+    
+                            <View>
+                               <View className='' style={{ }}>
+                                    <Section 
+                                        name={`Vendido por:  ${restaurant.name}`}
+                                        size="text-xl"
+                                        label=""
+                                        action={() => console.log("Clicou no veja mais")}
+                                    />
+                                </View>
+                               <View className='px-4'>
+                                    {restaurant.image && (
+                                        <Image
+                                            source={{ uri: restaurant.image }}
+                                            style={{ width: 70, height: 70, borderRadius: 50 }}
+                                        />
+                                    )}
+                                </View>
+                                
+                                <View className='px-4'>
+                                    <Text className='text-zinc-100 text-xl' style={{marginTop: 15}} >Mais perto de sua localização Atual:</Text>
+                                    <Text className='text-zinc-400 text-xs'>{restaurant.name} de Taguatinga </Text>
+                                    {restaurant.imageLocal && (
+                                        <Image
+                                            source={{ uri: restaurant.imageLocal }}
+                                            style={{
+                                                width: '100%',
+                                                height: 180,
+                                                borderRadius: 25,
+                                                marginTop: 20
+                                            }}
+                                        />
+                                    )}
+                                </View>
                             </View>
                         ) : (
                             <Text className='text-zinc-400'>Carregando detalhes do restaurante...</Text>
                         )}
-                    </View>
+                        
+                    {/* </View> */}
 
                 </ScrollView>
         </View>
